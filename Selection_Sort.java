f29d6ee780b69eac74e52029f1f5f4fd0ecff7a2
nripendra verma <113425608+nrii27@users.noreply.github.com>
2024-09-22 14:50:47 +0530

    Create Selection_Sort.java

---
+++ Selection_Sort.java
@@ -0,0 +1,24 @@
+
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        selectionSort(arr);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
